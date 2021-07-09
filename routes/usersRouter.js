514c8f00de76c897fb75131ad433f552b1befaa3
mamane19 <b.moussaama@alustudent.com>
2021-07-09 02:35:41 +0200

    ⚡ authentication

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -5,7 +5,7 @@ import { authenticate } from '../middlewares/auth.js';
 const usersRouter = express.Router();
 
 //Add a User - Sign Up - No need of Authentication.
-usersRouter.post("/signup", authenticate, signUp);
+usersRouter.post("/signup", signUp);
 
 //Add a User - Sign In - Authenticate
 usersRouter.post("/signin", signIn);
